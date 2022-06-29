6f40c7a5940967a6cf732e59bbf11cf4d731232f
guidolarosa <guido.glarosa@gmail.com>
2022-06-29 09:42:11 -0300

    Updated arch

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,17 @@
-import { Button } from "ui";
+// import { Button } from "api3-ui";
+import styled, { ThemeProvider, ThemeContext } from 'styled-components';
+import theme from '../../../themes/default';
+import { Flex } from 'rebass'
 
 export default function Docs() {
   return (
-    <div>
-      <h1>Docs</h1>
-      <Button />
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledAppRoot flexDirection={'column'} minHeight={'100vh'}>
+        <h1>Docs</h1>
+        {/* <Button variant='primary'>What a spanking button</Button> */}
+      </StyledAppRoot>
+    </ThemeProvider>
   );
 }
+
+const StyledAppRoot = styled(Flex)``;
