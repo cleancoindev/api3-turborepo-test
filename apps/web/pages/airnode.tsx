ee8f3fd4c9067162c29f1ae3ced04da3dc9b66eb
guidolarosa <guido.glarosa@gmail.com>
2022-06-29 10:38:05 -0300

    Minor update

--- apps/web/pages/airnode.tsx
+++ apps/web/pages/airnode.tsx
@@ -1,6 +1,6 @@
 // import { Button, Text } from "rebass";
 import { useState } from 'react';
-import { Button, Navbar } from 'api3-ui/dist';
+import { Button, Navbar } from 'api3-ui';
 import styled, { ThemeProvider, ThemeContext } from 'styled-components';
 import defaultTheme from 'api3-ui/dist/themes/default';
 
