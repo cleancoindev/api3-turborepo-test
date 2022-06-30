4ab6b45e38af17509ed7f3ee288177872b5a4255
guidolarosa <guido.glarosa@gmail.com>
2022-06-30 13:22:44 -0300

    Added storybook

---
+++ apps/docs/.storybook/preview.js
@@ -0,0 +1,17 @@
+// .storybook/preview.js
+
+export const parameters = {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#030303',
+        },
+        {
+          name: 'light',
+          value: 'white',
+        },
+      ],
+    },
+  };
